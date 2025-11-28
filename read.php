e7c27dd33dfe9e18502d38d541053edffe9f8ec5
scollp-esliceu <scollp@esliceu.net>
2025-11-28 16:32:28 +0100

    Commit fins es video 26

--- read.php
+++ read.php
@@ -4,25 +4,45 @@ $pageTitle = "Acerca";
 require __DIR__ . "/includes/header.php";
 $sql = "SELECT id, nom, telefon, email, missatge FROM contactos";
 $result=$conn->query($sql);
-
-echo "<pre>";
-var_dump($result);
-echo($result->num_rows);
-echo "</pre>";
 ?>
-<section>
+<link rel="stylesheet" href="/css/missatges.css">
+<section class="read-titol">
     <h2>Missatges</h2>
-    <p>Este sitio sirve como ejemplo para trabajar con Docker, PHP, MariaDB y phpMyAdmin.</p>
+    <p>Els missatges que enviin els usuaris sortiran a aquest registre</p>
 </section>
 
+<div class="Missatges-container llistat">
 <?php
     while($row = $result->fetch_assoc()){
-        echo $row['id'];
+        ?>
+        <div class="fila">
+                <div class="camp id">
+                    <?=$row['id'];?>
+                </div>
+                <div class="camp nom">
+                    <span class="etiqueta">NOM:</span>
+                    <?=$row['nom'];?>
+                </div>
+                <div class="camp email">
+                    <span class="etiqueta">CORREU:</span>
+                    <?=$row['email'];?>
+                </div>
+                <div class="camp telefon">
+                    <span class="etiqueta">TELÈFON:</span>
+                    <?=$row['telefon'];?>
+                </div>
+                <div class="camp missatge">
+                    <span class="etiqueta">MISSATGE:</span>
+                    <?=$row['missatge'];?>
+                </div>
+        </div>
+
+    <?php
     }
 
-    
 
 ?>
+</div>
 
 <?php
 require __DIR__ . "/includes/footer.php";
